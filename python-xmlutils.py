86cd8f1fb7e002ad247b14bb8375f9d8105820be
Albert Bowden <albert@sunlightfoundation.com>
2016-05-26 01:09:40 -0400

    foia.gov archived 2008-2012 data in XML. xmlutils.py conversion script for XML -> CSV/JSON. did not get CSV conversion functional. next up: batch converting

---
+++ python-xmlutils.py
@@ -0,0 +1,6 @@
+# Convert an XML document to a CSV file.
+xml2csv --input "samples/fruits.xml" --output "samples/fruits.csv" --tag "item"
+
+
+#Convert XML to JSON. xml2json supports hierarchies nested to any number of levels.
+xml2json --input "samples/fruits.xml" --output "samples/fruits.json"
